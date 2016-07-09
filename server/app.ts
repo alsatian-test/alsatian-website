b104658b9114de136f5f2117b8259bdf1b2b44f6
James Richford <jamesrichford@googlemail.com>
2016-07-09 23:53:19 +0100

    Successfully parse body

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,10 @@
 import * as Express from "express";
+import * as BodyParser from "body-parser";
 
 let app: Express.Application = Express();
+
+app.use(BodyParser.text());
+app.use(BodyParser.json());
 
 import TestService from "./test-service";
 
--- server/test-service.ts
+++ server/test-service.ts
@@ -1,16 +1,22 @@
 import * as Alsatian from "alsatian";
 import * as Express from "express";
+import * as TypeScript from "typescript";
 
 export default class TestService {
 
   public constructor(app: Express.Application) {
+
+
     app.post("/api/test", (request: Express.Request, response: Express.Response) => {
       let functionUnderTest: Function;
       let testRunnner = new Alsatian.TestRunner();
       let testFixture: any;
-      console.log(request.body);
-      eval("x = " + request.body + "; functionUnderTest = x.function; testFixture = x.testFixture");
 
+      console.log("testFixture = eval(" + request.body + ");");
+
+      eval("testFixture = eval(" + request.body + ");");
+
+      console.log(testFixture);
       // build fake test set
 
       // run fake test set
