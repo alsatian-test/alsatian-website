4a3798ac8ae3285c70fe547873d8a8672d3df24d
jamesrichford <jamesrichford@googlemail.com>
2016-07-11 17:58:22 +0100

    Store and run given js test

--- server/test-service.ts
+++ server/test-service.ts
@@ -1,6 +1,7 @@
 import * as Alsatian from "alsatian";
 import * as Express from "express";
 import * as TypeScript from "typescript";
+import FileSystem = require("fs");
 
 export default class TestService {
 
@@ -10,11 +11,14 @@ export default class TestService {
     app.post("/api/test", (request: Express.Request, response: Express.Response) => {
       let functionUnderTest: Function;
       let testRunnner = new Alsatian.TestRunner();
-      let testFixture: any;
+      let testFixtures: any = {};
+      FileSystem.writeFileSync("funky.js", JSON.parse(request.body).testFixture);
 
-      eval("testFixture = " + JSON.parse(request.body).testFixture);
+      let testSet = new Alsatian.TestSet("./funky.js");
 
-      testFixture.function();
+      console.log(testSet);
+
+      testRunnner.run(testSet);
       // build fake test set
 
       // run fake test set
