39a8657b2b9065f959ccd320d4f3590c374cc3e7
James Richford <jamesrichford@googlemail.com>
2016-10-14 18:58:53 +0100

    simple styling to menu

--- public/ts/menu-component.tsx
+++ public/ts/menu-component.tsx
@@ -10,19 +10,27 @@ export default class MenuComponent extends React.Component<any, any> {
 
    render() {
       return   <nav className={ this.props.open ? "navigation-menu open": "navigation-menu"}>
-                  <ul>
-                     <a href="/get-started">
-                        <li>Get started</li>
-                     </a>
-                     <a href="/tutorials">
-                        <li>Tutorials</li>
-                     </a>
-                     <a href="/blog">
-                        <li>Blog</li>
-                     </a>
-                     <a href="/documentation">
-                        <li>Documentation</li>
-                     </a>
+                  <ul className="menu-items">
+                     <li className="menu-item">
+                        <a href="/get-started">
+                           <div>Get started</div>
+                        </a>
+                     </li>
+                     <li className="menu-item">
+                        <a href="/tutorials">
+                           <div>Tutorials</div>
+                        </a>
+                     </li>
+                     <li className="menu-item">
+                        <a href="/blog">
+                           <div>Blog</div>
+                        </a>
+                     </li>
+                     <li className="menu-item">
+                        <a href="/documentation">
+                           <div>Documentation</div>
+                        </a>
+                     </li>
                   </ul>
                </nav>;
    }
