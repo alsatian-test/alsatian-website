0504c2148866b1429d871abe2c54a24a099927fe
James Richford <jamesrichford@googlemail.com>
2016-10-14 08:52:40 +0100

    add menu button to navigation bar

--- public/ts/navigation-bar-component.tsx
+++ public/ts/navigation-bar-component.tsx
@@ -10,6 +10,7 @@ export default class NavigationBarComponent extends React.Component<any, any> {
                      <h1>Alsatian</h1>
                      <a href="https://github.com/alsatian-test/alsatian">Github</a>
                   </div>
+                  <button id="menu-button">Menu</button>
                </nav>;
    }
 }
