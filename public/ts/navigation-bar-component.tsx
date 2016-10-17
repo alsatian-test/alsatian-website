9e7ec6d1e10d699a1b6a4fc7d8d03fafc7a59836
James Richford <jamesrichford@googlemail.com>
2016-10-17 18:25:14 +0100

    link back to home page

--- public/ts/navigation-bar-component.tsx
+++ public/ts/navigation-bar-component.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as Redux from "redux";
 import MenuComponent from "./menu-component";
+import { Link } from "react-router";
 
 export default class NavigationBarComponent extends React.Component<any, any> {
 
@@ -15,7 +16,9 @@ export default class NavigationBarComponent extends React.Component<any, any> {
    render() {
       return   <nav className="navigation-bar" onBlur={this.closeMenu.bind(this)}>
                   <div className="container">
-                     <h1>Alsatian</h1>
+                     <Link to="/">
+                        <h1>Alsatian</h1>
+                     </Link>
                      <a href="https://github.com/alsatian-test/alsatian">Github</a>
                   </div>
                   <button id="menu-button" onClick={this.toggleMenu.bind(this)}>Menu</button>
