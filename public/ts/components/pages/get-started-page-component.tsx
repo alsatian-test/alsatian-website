85e11396090a5f48e3ca6dad512ec12c7b631775
James Richford <jamesrichford@googlemail.com>
2016-10-17 18:35:03 +0100

    create structure for get started page

--- public/ts/components/pages/get-started-page-component.tsx
+++ public/ts/components/pages/get-started-page-component.tsx
@@ -7,8 +7,24 @@ export default class GetStartedPageComponent extends React.Component<any, any> {
    }
 
    render() {
-      return <div>
-               <h1>Get started</h1>
-             </div>;
-   }
+      return   <div>
+                  <section className="billboard findbettername">
+                     <div className="container">
+                        <h1>Get started</h1>
+                     </div>
+                  </section>
+                  <section className="container">
+                     <h2>Getting your setup awesome</h2>
+                  </section>
+                  <section className="container">
+                     <h2>Writing tests</h2>
+                  </section>
+                  <section className="container">
+                     <h2>Coverage, TAP Reporters, Debugging, End to End Testing</h2>
+                  </section>
+                  <section className="container">
+                     <h2>Next steps and where to find them</h2>
+                  </section>
+               </div>;
+      }
 }
