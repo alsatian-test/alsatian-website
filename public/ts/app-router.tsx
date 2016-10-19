4d7ce1f35d1c3f324eaa1fdab8055be9e84bc2e5
James Richford <jamesrichford@googlemail.com>
2016-10-19 19:07:38 +0100

    add other missing pages

--- public/ts/app-router.tsx
+++ public/ts/app-router.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import AppComponent from "./app-component";
-import NavigationBarComponent from "./navigation-bar-component";
 import GetStartedPageComponent from "./components/pages/get-started-page-component";
 import HomePageComponent from "./components/pages/home-page-component";
 import NotFoundPageComponent from "./components/pages/not-found-page-component";
 import TutorialsPageComponent from "./components/pages/tutorials-page-component";
+import BlogPageComponent from "./components/pages/blog-page-component";
+import DocumentationPageComponent from "./components/pages/documentation-page-component";
+
 
 export default class AppRouter extends React.Component<any, any> {
    render() {
@@ -14,6 +16,8 @@ export default class AppRouter extends React.Component<any, any> {
                <Route path="/" component={AppComponent}>
                   <IndexRoute component={HomePageComponent} />
                   <Route path="get-started" component={GetStartedPageComponent} />
+                  <Route path="blog" component={BlogPageComponent} />
+                  <Route path="documentation" component={DocumentationPageComponent} />
                   <Route path="tutorials" component={TutorialsPageComponent} />
                   <Route path="*" component={NotFoundPageComponent} />
                </Route>
