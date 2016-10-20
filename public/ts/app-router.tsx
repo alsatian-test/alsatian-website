7d4a003ab8508a440d5fffcbffd11742425b775e
James Richford <jamesrichford@googlemail.com>
2016-10-20 18:57:10 +0100

    remove get started page

--- public/ts/app-router.tsx
+++ public/ts/app-router.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 import AppComponent from "./app-component";
-import GetStartedPageComponent from "./components/pages/get-started-page-component";
 import HomePageComponent from "./components/pages/home-page-component";
 import NotFoundPageComponent from "./components/pages/not-found-page-component";
 import TutorialsPageComponent from "./components/pages/tutorials-page-component";
@@ -16,7 +15,6 @@ export default class AppRouter extends React.Component<any, any> {
       return <Router history={browserHistory}>
                <Route path="/" component={AppComponent}>
                   <IndexRoute component={HomePageComponent} />
-                  <Route path="get-started" component={GetStartedPageComponent} />
                   <Route path="blog" component={BlogPageComponent} />
                   <Route path="documentation" component={DocumentationPageComponent} />
                   <Route path="tutorials/" component={TutorialsPageComponent}>
