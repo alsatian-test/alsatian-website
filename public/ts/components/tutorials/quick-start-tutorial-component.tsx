40199a9a2be697d6be0f5c1fce1564dfc628ce65
jamesrichford <jamesrichford@googlemail.com>
2016-11-09 13:35:13 +0000

    add transition for steps

--- public/ts/components/tutorials/quick-start-tutorial-component.tsx
+++ public/ts/components/tutorials/quick-start-tutorial-component.tsx
@@ -6,7 +6,41 @@ export default class QuickStartTutorialComponent extends React.Component<any, an
 
    public constructor() {
       super();
+
+      this.state = { _transparent: true };
    }
+
+   private _transparent = true;
+   private _something: HTMLElement;
+   
+   public componentDidMount() {
+      addEventListener('DOMContentLoaded', this._checkIsVisible.bind(this), false);
+      addEventListener('load', this._checkIsVisible.bind(this), false);
+      addEventListener('scroll', this._checkIsVisible.bind(this), false);
+      addEventListener('resize', this._checkIsVisible.bind(this), false);
+   }
+   
+   private _checkIsVisible(event: Event) {
+    if (!this.state._transparent) {
+      return;
+    }
+    if (!this._something) {
+      return;
+    }
+
+    let rect = this._something.getBoundingClientRect();
+
+    if (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.top + ((rect.bottom - rect.top) / 2) <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    ) {
+      this.setState({ _transparent: false});
+
+      //this._filterSkills();
+    }
+  }
 
    public static get defaultProps() {
       return { title: "Quick Start" }
@@ -19,8 +53,9 @@ export default class QuickStartTutorialComponent extends React.Component<any, an
                         <h3>Install Alsatian</h3>
                         <pre><code className="powershell">npm install alsatian --save-dev</code></pre>
                      </li>
-                     <li>
+                     <li data-transparent={this.state._transparent} ref={(c) => this._something = c}>
                         <h3>Write your first test</h3>
+                        <hr></hr>
                         <p>Simply create a spec file paste this code and save it.</p>
                         <CodeBlock>
                            {
