d6cc093d0df8f297842110d3a95c78a87ad4c854
James Richford <jamesrichford@googlemail.com>
2016-11-24 07:46:39 +0000

    add first alsatian test

---
+++ public/ts/navigation-bar-component.spec.ts
@@ -0,0 +1,12 @@
+import { Test, Expect, TestFixture } from "alsatian";
+import NavigationBarComponent from "./navigation-bar-component";
+
+@TestFixture("Navigation Bar Component Tests")
+export default class NavigationBarComponentTests {
+    @Test("Menu is not open by default")
+    public menuNotOpenByDefault() {
+        const navigationBar = new NavigationBarComponent();
+
+        Expect(navigationBar.state.isOpen).toBe(false);
+    }
+}
