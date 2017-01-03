d97148dab34a64fbec29c214603d75c1ec982567
James Richford <jamesrichford@googlemail.com>
2017-01-03 21:38:42 +0000

    add info to about page

--- public/ts/components/pages/about-page-component.tsx
+++ public/ts/components/pages/about-page-component.tsx
@@ -11,8 +11,21 @@ export default class AboutPageComponent extends React.Component<any, any> {
                   <section className="billboard findbettername">
                      <div className="container">
                         <h1>About</h1>
-                        <p>This will have info about Alsatian</p>
+                        <p>Alsatian is different</p>
                      </div>
+                  </section>
+                  <section className="container">
+                     <h2>What is Alsatian</h2>
+                     <p>Something about next gen stuff</p>
+                     <h2>Acknowledgements</h2>
+                     <p>Alsatian is free and open source and couldn't survive without the wonderful people who provide freebees for the Open Source community! We use the following services and are proud to:</p>
+                     <ul>
+                        <li>GitHub</li>
+                        <li>NPM</li>
+                        <li>Travis CI</li>
+                     </ul>
+                     <p>Furthermore, we couldn't be where we are without community support. Whether that be <a href="https://github.com/alsatian-test/alsatian/graphs/contributors">contributors</a> or people raising issues, starring the repository or just using Alsatian - we love you all.</p>
+                     <p>Icons on this site have been provided by...</p>
                   </section>
                </div>;
       }
