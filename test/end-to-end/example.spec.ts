7877a31768117aad3939aa0bf1eaae9b3b79d96b
jamesrichford <jamesrichford@googlemail.com>
2017-03-09 09:23:23 +0000

    update example end to end test

--- test/end-to-end/example.spec.ts
+++ test/end-to-end/example.spec.ts
@@ -5,62 +5,17 @@ import { TestFixture, AsyncTest, Expect } from "alsatian";
 export default class ExampleEndToEndTests {
 
     @AsyncTest("check nav title spelled correctly")
-    public checkNavTitle() {
+    public async checkNavTitle() {
         const driver = new Builder().
             withCapabilities(Capabilities.chrome()).
             build();
         
-        return new Promise((resolve, reject) => {
-            driver.get("localhost:3000").then(() => {
-                const headingTitle = driver.findElement(By.css("nav h1"));
+        await driver.get("localhost:3000");
 
-                headingTitle.getText().then(headingText => {
-                    Expect(headingText).toBe("Alsatian");
-                });
+        const headingTitle = driver.findElement(By.css("nav h1"));
 
-                resolve();
-            });
-        })
+        const headingText = await headingTitle.getText();
+        
+        Expect(headingText).toBe("Alsatian");
     }
-}
-
-export class Promise<T> {
-
-   private _resolveCallback: (resolvedValue?: T) => any = () => {};
-   private _rejectCallback: (error: Error) => any = () => {};
-
-   public constructor(asyncFunction: (resolve: (resolvedValue?: T) => any, reject: (error: Error) => any) => any) {
-      setTimeout(() => {
-         try {
-            asyncFunction(this._resolve.bind(this), this._reject.bind(this));
-         }
-         catch (error) {
-            this._rejectCallback(error);
-         }
-      });
-   }
-
-   private _resolve(resolvedValue?: T) {
-      try {
-         this._resolveCallback(resolvedValue);
-      }
-      catch (error) {
-         this._reject(error);
-      }
-
-   }
-
-   private _reject(error: Error) {
-      this._rejectCallback(error);
-   }
-
-   public then(callback: (resolvedValue?: T) => any) {
-      this._resolveCallback = callback;
-      return this;
-   }
-
-   public catch(callback: (error: Error) => any) {
-      this._rejectCallback = callback;
-      return this;
-   }
 }
